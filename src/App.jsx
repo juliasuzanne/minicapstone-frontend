6aef1365fb1b64dcd4affcecd5b3a5b845ea3ed7
Julia Grimes <julia.s.grimes@gmail.com>
2022-12-15 21:48:15 -0500

    Add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Home } from "./Home";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div>
